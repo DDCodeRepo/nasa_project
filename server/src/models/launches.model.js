392f8f2cbedca7fba5f5139a1963ce6e5338a4fb
Debjit Dhar <debjit8dhar@gmail.com>
2022-03-20 14:17:14 +0530

    end of launches module

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,22 +1,48 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
     flightNumber: 100,
     mission: 'Kepler Exploration X',
     rocket: 'Explorer IS1',
     launchDate: new Date('December 27, 2030'),
-    destination: 'Kepler-442 b',
+    target: 'Kepler-442 b',
     customer: ['ZTM', 'NASA'],
     upcoming: true,
     success: true,
 };
 
-launches.set(launch.flightNumber, launch);
+function existsLaunchWithId(launchId){
+    return launches.has(launchId);
+}
 
 function getAllLaunches(){
     return Array.from(launches.values());
 }
 
+launches.set(launch.flightNumber, launch);
+
+function addNewLaunch(launch){
+    latestFlightNumber++;
+    launches.set(latestFlightNumber, Object.assign(launch, {
+        success: true,
+        upcoming: true,
+        customers: ['ZTM', 'NASA'],
+        flightNumber: latestFlightNumber,
+    }));
+}
+
+function abortLaunchById(launchId){
+    const aborted = launches.get(launchId);
+    aborted.upcoming = false;
+    aborted.success = false;
+    return aborted;
+}
+
 module.exports = {
     getAllLaunches,
+    addNewLaunch,
+    existsLaunchWithId,
+    abortLaunchById,
 };
